8cc86d5e87b6166ccbc28821ecb3da61cf4cdf07
Pedro Gregorio <pedrogregorio@jeraapps.com>
2021-09-10 22:11:11 -0400

    creating the project structure

---
+++ migration/1631288968010-user.ts
@@ -0,0 +1,10 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class user1631288968010 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { Connection } from 'typeorm';
+import { PostsModule } from './posts/posts.module';
+import { Post } from './posts/entities/post.entity';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'gregorio',
+      password: 'secret123',
+      database: 'api_nest_test',
+      entities: [User, Post],
+      migrations: ['migration/*.js'],
+      synchronize: true,
+      cli: {
+        migrationsDir: 'migration',
+      },
+    }),
+    UsersModule,
+    PostsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
